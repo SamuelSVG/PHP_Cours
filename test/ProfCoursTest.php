e3e8f0e2b2f7f6832640d59ca0a0e5cb799e9c3d
Sébastien Vandeneede <79372718+SebVde@users.noreply.github.com>
2024-11-21 15:32:52 +0100

    Update ProfCoursTest.php

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -340,7 +340,7 @@ class ProfCoursTest extends TestCase
         $this->assertTrue($val, "Update du prof num $idProf ...\n");
 
         // Cours
-        $cours = new Cours($this->intitule, $this->duree, $this->prof);
+        $cours = new Cours($this->intitule, $this->duree, 10);
         $val = $cours->updateOne($conn, $idCours);
         $expected_cours_str = $cours->__toString();
         $record_cours = Cours::printOne($conn, $idCours);
